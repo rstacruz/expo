0693baa55cc9023d7adf85c79f036bdb02297e62
Rico Sta. Cruz <hi@ricostacruz.com>
2013-05-23 16:09:40 +0800

    Assets.

--- expo-connect_assets/index.js
+++ expo-connect_assets/index.js
@@ -1,6 +1,8 @@
 module.exports = function(app) {
-  app.on('load:after', function() {
-    console.log('.');
-    app.use(require('connect-assets')());
+  app.on('load:after', function(app) {
+    app.use(require('connect-assets')({
+      src: app.path('assets'),
+      buildDir: app.path('public')
+    }));
   });
 };
--- sample/routes/home.js
+++ sample/routes/home.js
@@ -1,5 +1,5 @@
 module.exports = function(app) {
   app.get('/', function(req, res) {
-    res.send("Hello");
+    res.render('index', {});
   });
 };
