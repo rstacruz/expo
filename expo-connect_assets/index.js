e97f4957e1936926942b4ef8c6db2f03748c0692
Rico Sta. Cruz <hi@ricostacruz.com>
2013-05-23 16:18:55 +0800

    Assets config.

--- expo-connect_assets/index.js
+++ expo-connect_assets/index.js
@@ -1,8 +1,26 @@
+var extend = require('util')._extend;
+
 module.exports = function(app) {
   app.on('load:after', function(app) {
-    app.use(require('connect-assets')({
+    var options = {
       src: app.path('assets'),
       buildDir: app.path('public')
-    }));
+    };
+
+    // Get config from app.get.
+    if (typeof app.get('assets') === 'object') {
+      extend(options, app.get('assets'));
+    }
+
+    app.use(require('connect-assets')(options));
+  });
+
+  app.on('cli', function(app, cli) {
+    cli
+      .command('assets:precompile')
+      .description('Precompiles asset files')
+      .action(function() {
+        console.log('..');
+      });
   });
 };
--- sample/initializers/app.js
+++ sample/initializers/app.js
@@ -1,25 +1,21 @@
 var express = require('express');
 
 module.exports = function(app) {
+  app.configure(function() {
+    app.set('view engine', 'jade');
+    app.set('views', app.path('views'));
+    app.use(express['static'](app.path('public')));
+    app.use(express.favicon());
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+  });
+
   app.configure('development', function() {
+    app.use(express.logger('dev'));
+    app.use(express.errorHandler());
   });
 
   app.configure(function() {
-    // Views
-    app.set('view engine', 'jade');
-    app.set('views', app.path('views'));
-
-    // Public folder
-    app.use(express['static'](app.path('public')));
-
-    app.use(express.favicon());
-    if (app.get('env') === 'development') app.use(express.logger('dev'));
-    app.use(express.bodyParser());
-    app.use(express.methodOverride());
     app.use(app.router);
-  });
-
-  app.configure('development', function() {
-    app.use(express.errorHandler());
   });
 };
