aa50d14b836eb6f39878292fb5596b19921494fc
Rico Sta. Cruz <hi@ricostacruz.com>
2013-05-30 01:24:07 +0800

    Generate cryptographically-secure secret strings.

--- expo/lib/gen/renderer.js
+++ expo/lib/gen/renderer.js
@@ -6,19 +6,29 @@ var Renderer = module.exports = function(options) {
     text: function(fpath) {
       return readTemplate(fpath);
     },
+
     js: function(fpath) {
       return readTemplate(fpath);
     },
+
     readme: function(fpath) {
       return this.text(fpath)
         .replace(/PROJECT/g, options.project);
     },
+
     jade: function(fpath) {
       var str = this.text(fpath);
       if (!options.assets)
         str = str.replace(/\n.*( js| css).*\n/g, '\n');
       return str;
     },
+
+    secret: function() {
+      var crypto = require('crypto');
+      var string = crypto.randomBytes(128).toString('base64');
+      return 'production: "'+string+'"\n';
+    },
+
     appjs: function(fpath) {
       var str = this.js(fpath);
       if (!options.db)
@@ -27,7 +37,8 @@ var Renderer = module.exports = function(options) {
         str = str.replace(/^.*'expo-connect_assets'.*\n/mg, '\n');
       return str;
     },
-    package: function(fpath) {
+
+    'package': function(fpath) {
       var str = this.readme(fpath);
       if (!options.db)
         str = str.replace(/^.*"(expo-sequelize|pg|mysql|sqlite3)".*\n/mg, '\n');
@@ -37,9 +48,9 @@ var Renderer = module.exports = function(options) {
         str = str.replace(/^.*"(test|mocha|chai|supertest)".*\n/mg, '\n');
       str = JSON.stringify(JSON.parse(str), null, 2);
       return str;
-    },
+    }
   };
-}
+};
 
 function readTemplate(fpath) {
   var fullpath = path.resolve(__dirname, '../../template', fpath);
