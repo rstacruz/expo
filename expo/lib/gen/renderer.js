62953e3ac4d0c2524e758ec661ba54032872559f
Rico Sta. Cruz <hi@ricostacruz.com>
2013-05-29 21:42:03 +0800

    Gen: Refactor renderer.

---
+++ expo/lib/gen/renderer.js
@@ -0,0 +1,47 @@
+var fs = require('fs');
+var path = require('path');
+
+var Renderer = module.exports = function(options) {
+  return {
+    text: function(fpath) {
+      return readTemplate(fpath);
+    },
+    js: function(fpath) {
+      return readTemplate(fpath);
+    },
+    readme: function(fpath) {
+      return this.text(fpath)
+        .replace(/PROJECT/g, options.project);
+    },
+    jade: function(fpath) {
+      var str = this.text(fpath);
+      if (!options.assets)
+        str = str.replace(/\n.*( js| css).*\n/g, '\n');
+      return str;
+    },
+    appjs: function(fpath) {
+      var str = this.js(fpath);
+      if (!options.db)
+        str = str.replace(/^.*'expo-sequelize'.*\n/mg, '\n');
+      if (!options.assets)
+        str = str.replace(/^.*'expo-connect_assets'.*\n/mg, '\n');
+      return str;
+    },
+    package: function(fpath) {
+      var str = this.readme(fpath);
+      if (!options.db)
+        str = str.replace(/^.*"(expo-sequelize|pg|mysql|sqlite3)".*\n/mg, '\n');
+      if (!options.assets)
+        str = str.replace(/^.*"(expo-connect_assets|stylus|nib)".*\n/mg, '\n');
+      if (!options.tests)
+        str = str.replace(/^.*"(test|mocha|chai|supertest)".*\n/mg, '\n');
+      str = JSON.stringify(JSON.parse(str), null, 2);
+      return str;
+    },
+  };
+}
+
+function readTemplate(fpath) {
+  var fullpath = path.resolve(__dirname, '../../template', fpath);
+  return fs.readFileSync(fullpath).toString();
+}
