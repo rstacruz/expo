ee466c3ef1f5f6638e576fc4c7611fff38ba920c
Rico Sta. Cruz <hi@ricostacruz.com>
2013-05-30 23:38:54 +0800

    Expo gen: generate assets config.

--- expo/template/init.assets.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(app) {
-  app.configure(function() {
-    // List of assets to be precompiled when running `./run assets:precompile`.
-    app.set('assets precompiled', {
-      js: ['application'],
-      css: ['application']
-    });
-  });
-};
