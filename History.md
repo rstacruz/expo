a6e5ef0e9214e02143ee3fa53bf692c4b565fa5a
Rico Sta. Cruz <hi@ricostacruz.com>
2013-05-30 11:12:13 +0800

    Gen: fix npm install.

--- History.md
+++ History.md
@@ -6,6 +6,7 @@ App generator:
  * New projects now have a `repository` in package.json.
  * New projects now default to version "0.0.1".
  * Fix the default tasks.
+ * Fix the 'npm install' error.
 
 Expo:
 
--- expo/lib/gen/helpers.js
+++ expo/lib/gen/helpers.js
@@ -3,9 +3,9 @@
  * Executes a given shell command.
  */
 
-function exec(cmd, callback) {
+exports.exec = function(cmd, callback) {
   var spawn = require('child_process').spawn;
-  var proc = spawn('sh', ['-c', cmd].concat(args), { stdio: 'inherit', customFds: [0, 1, 2] });
+  var proc = spawn('sh', ['-c', cmd].concat(cmd), { stdio: 'inherit', customFds: [0, 1, 2] });
   proc.on('exit', function(code){
     if (code > 0) return callback(code);
     return callback();
