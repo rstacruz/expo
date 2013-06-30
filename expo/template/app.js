2c8addc6566b0056d9e1f1087803f9379c43b228
Rico Sta. Cruz <hi@ricostacruz.com>
2013-07-01 01:26:21 +0800

    Template: make the app listen with 'node app.js'.

--- expo/template/app.js
+++ expo/template/app.js
@@ -2,3 +2,10 @@ var app = module.exports = require('express')();
 require('expo')(app, __dirname);
 require('expo-sequelize')(app);
 require('expo-connect_assets')(app);
+
+// Listen when invoked as `node .` or `node app.js`
+if (!module.parent) {
+  var port = process.env.PORT || 3000;
+  app.log.debug("Listening at port " + port);
+  app.load().listen(port);
+}
